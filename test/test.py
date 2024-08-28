b2f60ab37bdf6de278f428e2cc7e04f8f6405be5
user.email <0kq@0kqnet.work>
2024-08-28 15:26:02 +0900

    update gitignore

--- test/test.py
+++ test/test.py
@@ -6,7 +6,8 @@ import asyncio
 
 async def main():
     client = EnkaNetworkAPI(lang=Language.JP)
-    await client.fetch_user_by_uid(800857830)
+    resp = await client.fetch_user_by_uid(800857830)
+    print(resp.player.tower_act)
     print("fetch user OK")
     await client.fetch_user_by_username("tankload")
     print("fetch hoyos OK")
